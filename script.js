e733fc148c11009b598fd946f08e176fc137fa1c
SpiffyJohnson <135999968+SpiffyJohnson@users.noreply.github.com>
2026-01-03 15:55:42 -0600

    Moved more <script> tags into the script.js file

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-function FoundSecret(AnimationToShow = 99) {
-    switch (AnimationToShow) {
-        case 0:
-            ShowAnimation('mountainpattern', 'img/homepattern.gif');
-            break;
-        default:
-            return;
-    }
-    
-    if (localStorage.IndexSecret == null) {
-        if (localStorage.FoundSecret1 == null) {
-            alert("You found a Secret Monster!\n\nYou can view it in the BOOK, IMAGES, and SOUNDS pages!");
-            localStorage.FoundSecret1 = "Yes";
-            localStorage.AmountOfSecretEnemies = 4;
-        } else if (localStorage.FoundSecret2 == null) {
-            alert("You found a Secret Monster!\n\nYou can view it in the BOOK, IMAGES, and SOUNDS pages!");
-            localStorage.FoundSecret2 = "Yes";
-            localStorage.AmountOfSecretEnemies = 3;
-        } else if (localStorage.FoundSecret3 == null) {
-            alert("You found a Secret Monster!\n\nYou can view it in the BOOK, IMAGES, and SOUNDS pages!");
-            localStorage.FoundSecret3 = "Yes";
-            localStorage.AmountOfSecretEnemies = 2;
-        } else if (localStorage.FoundSecret4 == null) {
-            alert("You found a Secret Monster!\n\nYou can view it in the BOOK, IMAGES, and SOUNDS pages!");
-            localStorage.FoundSecret4 = "Yes";
-            localStorage.AmountOfSecretEnemies = 1;
-        } else if (localStorage.FoundSecret5 == null) {
-            alert("You found a Secret Monster!\n\nYou can view it in the BOOK, IMAGES, and SOUNDS pages!");
-            localStorage.FoundSecret5 = "Yes";
-            localStorage.AmountOfSecretEnemies = 0;
-        }
-        localStorage.IndexSecret = 1;
-    }
-}
-
 function ShowAnimation(element, source) {
     var image = document.getElementById(element);
     image.src = source;
@@ -43,3 +8,35 @@ function coffeeOrder() {
     alert("If you could just grab me a cup of coffee with caramel, creamer, and two spoons of sugar, that'd be great.\n\n~Website Creator");
 }
 
+// From secret.html
+function ShowSecret() {
+    if (localStorage.FoundSecret == null) {
+        var image = document.getElementById("hiddenimg");
+        image.src = "img/monsters/sprite_f.gif";
+        localStorage.FoundSecret = 1;
+        Sound = new Audio('audio/surprise.mp3');
+        Sound.play();
+        Pitch = new Audio('audio/surprisepitch.mp3');
+        Pitch.loop = true;
+        Pitch.play();
+        CorruptScreen();
+        alert("He found you, [InvalidUsername]! He'll look for you in the IMAGES tab!");
+    }
+}
+function FoundSecret() {
+    if (localStorage.FoundSecret != null) {
+        var image = document.getElementById("hiddenimg");
+        image.src = "img/decoy.png";
+        CorruptScreen();
+    }
+}
+
+function CorruptScreen() {
+    document.getElementById("main").style.backgroundImage = "url('img/error.png')";
+    document.getElementById("title").innerText = "><?a$%^*)_}{";
+    document.getElementById("top").innerText = "\n";
+    document.body.style.backgroundImage = "url('img/error2.png')";
+    document.getElementById("header").style.backgroundImage = "url('img/error.png')";
+    document.getElementById("navsetup").style.backgroundImage = "url('img/error3.png')";
+    document.getElementById("foot").style.backgroundImage = "url('img/error4.png')";
+}
